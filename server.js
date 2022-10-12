2edd69e5cb24781f0cfe9430e0951596b8e42d80
jesse-morales <moral.jesse@outlook.com>
2022-10-12 09:30:38 -0600

    Add title and New Article button

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.set('view engine', 'ejs')
 
 app.use('/articles',articleRouter)
 app.get('/', (req, res) => {
+    const articles = [{
+        title: 'Test Article',
+        createdAt: Date.now(),
+        description: 'Test Description'
+    }]
     res.render('index', {articles: articles})
 })
 
