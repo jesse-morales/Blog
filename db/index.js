ab65fbe345700896bf9c3a17957ee8a14a8dd3fd
jesse-morales <moral.jesse@outlook.com>
2022-11-11 19:12:09 -0700

    Configure DB

--- db/index.js
+++ db/index.js
@@ -1,17 +1,24 @@
-/*import postgres from 'postgres'
+const { Client } = require("pg")
+const dotenv = require("dotenv")
+dotenv.config()
 
-const sql = postgres
-
-export default sql*/
-
-const { Pool} = require('pg')
-
-/*const client = new Client({
-    user:  'blog_user',
-    host: 'localhost',
-    database: 'mydb',
-    password: 'encartado',
-    port: 5000,
-})*/
-const res = await Pool.query('SELECT NOW()')
-await Pool.end()
+const connectDb = async () => {
+    try {
+        const client = new Client({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT
+        })
+ 
+        await client.connect()
+        const res = await client.query('SELECT * FROM some_table')
+        console.log(res)
+        await client.end()
+    } catch (error) {
+        console.log(error)
+    }
+}
+ 
+connectDb()
